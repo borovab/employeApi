a334338fa8144be59e27220f6a6f546f0934bda8
Beqir Borova <beqir10@gmail.com>
2025-12-02 01:52:50 +0100

    update

--- backend/DTOs/EmployeeDtos.cs
+++ backend/DTOs/EmployeeDtos.cs
@@ -1,6 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-using EmployeeApi.Models;
-
 namespace EmployeeApi.DTOs;
 
 public class EmployeeDto
@@ -9,16 +6,21 @@ public class EmployeeDto
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
     public DateTime DateOfBirth { get; set; }
-    public EducationLevel EducationLevel { get; set; }
-    public int Age { get; set; }
+    public string EducationLevel { get; set; } = "";
 }
 
 public class CreateEmployeeDto
 {
-    [Required] public string FirstName { get; set; } = "";
-    [Required] public string LastName { get; set; } = "";
-    [Required] public DateTime DateOfBirth { get; set; }
-    [Required] public EducationLevel EducationLevel { get; set; }
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public DateTime DateOfBirth { get; set; }
+    public string EducationLevel { get; set; } = "";
 }
 
-public class UpdateEmployeeDto : CreateEmployeeDto {}
+public class UpdateEmployeeDto
+{
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public DateTime DateOfBirth { get; set; }
+    public string EducationLevel { get; set; } = "";
+}
