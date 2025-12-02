a47fd4ea17c5293bd6dae3e78758d17a99a1600b
Beqir Borova <beqir10@gmail.com>
2025-12-02 01:22:02 +0100

    update automigration

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,19 +3,30 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Controllers
 builder.Services.AddControllers();
 
-// EF Core InMemory
+// PostgreSQL Database
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("EmployeesDb"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// AUTO-MIGRATION – krijon tabelat në PostgreSQL nese mungojnë
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+// Swagger (Production + Development)
 app.UseSwagger();
 app.UseSwaggerUI();
+
 app.MapControllers();
 
 app.Run();
